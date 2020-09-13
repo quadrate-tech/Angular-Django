d3dcc80629f9e799f42ce3425320df88f5ffc8e6
Apeksha <apeksha.2018562@iit.ac.lk>
2020-09-14 00:41:34 +0530

    Add files via upload
    updates

--- qts_app/qts_app/router.py
+++ qts_app/qts_app/router.py
@@ -1,13 +1,17 @@
-from qts_api.viewsets import ad_typeViewSet
-from rest_framework import routers
-
+from qts_api.viewsets import ad_typeViewSet, promotion_packageViewSet,  feedbackViewSet
+from qts_api.viewsets import ad_listingViewSet
+from qts_api.viewsets import districtViewSet
 from qts_api.viewsets import paymentViewSet
+from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('ad_type', ad_typeViewSet)
-
-router = routers.DefaultRouter()
+router.register('promotion_package', promotion_packageViewSet)
+router.register('feedback', feedbackViewSet)
+router.register('ad_listing', ad_listingViewSet)
+router.register('district', districtViewSet)
 router.register('payment', paymentViewSet)
+
 
 # localhost:p/api/employee/5
 # GET, POST, PUT, DELETE
