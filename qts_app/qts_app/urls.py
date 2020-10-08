176a434af0d6ace5e313dddd6cee62a8f7654af2
pankajan05 <pankajan05@gmail.com>
2020-10-08 11:12:00 +0530

    generate token successfully

--- qts_app/qts_app/urls.py
+++ qts_app/qts_app/urls.py
@@ -6,6 +6,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/auth/token',TokenObtainPairView.as_view,name="auth_token"),
-    path('api/auth/refresh',TokenRefreshView.as_view,name="auth_refresh_token")
+    path('api/auth/token', TokenObtainPairView.as_view(), name="auth_token"),
+    path('api/auth/refresh', TokenRefreshView.as_view(), name="auth_refresh_token")
 ]
