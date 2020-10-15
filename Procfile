86d736f151a76252c2ebdea9055140dd3d6e66f5
pankajan05 <pankajan05@gmail.com>
2020-10-15 10:34:05 +0530

    delete

--- Procfile
+++
@@ -1,4 +0,0 @@
-release: python manage.py makemigrations --no-input
-release: python manage.py migrate --no-input
-
-web: gunicorn qts_app.wsgi
--- qts_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .router import router
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
-]
